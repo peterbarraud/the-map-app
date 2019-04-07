f280fcaab2596980cf5d26c212f35150037c587a
Peter G.A. Barraud <barraud@adobe.com>
2019-04-07 10:30:30 +0530

    sql queries

--- the.dr.nefario.backside/services/rest.api.php
+++ the.dr.nefario.backside/services/rest.api.php
@@ -11,7 +11,11 @@
     echo json_encode($users);
   }
 
-  function getestablishmentbyname($name) {
+  function searchforestbynamelike($name) {
+    // select e.id, e.name, e.address, e.phone from establishment e, area a where e.areaid = a.id and a.id = 1 and e.name like 'ABAN%';
+    // select * from establishment where id in (select estid from est_cat where catid = (select catid from est_cat where estid = 478)) and areaid = 1 and name not like 'ABAN%';
+    // select e.name as ename, e.address, e.phone, c.name, c.id from establishment e, category c, est_cat ec where e.id = ec.estid and c.id = ec.catid and e.areaid = 3 and e.name like 'ABAN%';
+    // select e.name as ename, e.address, e.phone, c.name, c.id from establishment e, category c, est_cat ec where e.id = ec.estid and c.id = ec.catid and e.areaid = 3 and c.id in (2, 9, 12);
     require_once('objectlayer/establishmentcollection.php');
     $establishments = new establishmentcollection(array("name" => $name));
     echo json_encode($establishments);

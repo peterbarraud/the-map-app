cfd3d553105dd5fe6de316117447a8a8553dac86
Peter G.A. Barraud <barraud@adobe.com>
2019-04-12 11:20:14 +0530

    semi-intelligent search - wip

--- the.dr.nefario.backside/services/objectlayer/establishmentcollection.php
+++ the.dr.nefario.backside/services/objectlayer/establishmentcollection.php
@@ -24,20 +24,49 @@ class establishmentcollection extends objectcollectionbase {
         return array('items' => $items, 'length' => $length);              
     }
 
+    private static function log_search_results($str){
+        file_put_contents('./logs/GetSearchResults.log', $str . "\n", FILE_APPEND);
+    }
     public static function GetSearchResults($search_q, $areaid){
+        file_put_contents('./logs/GetSearchResults.log', "");
+        
         require_once ('datalayer.php');
         $dataLayer = DataLayer::Instance();
         $estabsandcats = $dataLayer->GetAllEstabsAndCatsByArea($areaid);
         $estabs = array();
         $cats = array();
-        file_put_contents('./logs/' . __FUNCTION__ . '.log', "$search_q" . "\n");
         foreach($estabsandcats as $estabsandcat){
           $estabs[$estabsandcat['ename']] = $estabsandcat['eid'];
           $cats[$estabsandcat['cname']] = $estabsandcat['cid'];
         }
-        foreach($cats as $key => $value){
-          file_put_contents('./logs/' . __FUNCTION__ . '.log', $key . '=>' . $value . "\n", FILE_APPEND);
+        // to allow for free-flowing text
+        // 1. split the search_q into words
+        //      $words = preg_split ("/\s+/", $search_q);
+        // 2. extract out linear combos of words (not the math meaning)
+        //      example:
+        //          "my hello world" => gives these combos
+        //          ["my", "my hello", "my hello world", "hello", "hello world", "world"]
+        // 3. use preg_grep to search each linear combo inside both the estab and cat arrays
+        // 4. the best result is the closest match
+        //      example:
+        //          searching for "bike repair" means:
+        //              "bike repair" is better than "motor bike repair"
+        
+        // 1. split the search_q into words
+        self::log_search_results($search_q);
+        $words = preg_split ("/\s+/", $search_q);
+        // $top_of_words_array = ;
+        foreach (range(0, sizeof($words)-1) as $i){
+            foreach(range(1, sizeof($words) - $i) as $j){
+                // self::log_search_results("$i, $j");
+                $slice = array_slice($words, $i, $j);
+                self::log_search_results(implode(' ', $slice));
+            }
+            self::log_search_results("=============");
         }
+        
+
+        $estab_matches = preg_grep("/$search_q/i", array_keys($estabs));
         return $estabsandcats;
     }
 }
